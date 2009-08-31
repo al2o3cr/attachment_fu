c4a16eb5432a818579d3982c6496103404a78000
Matt Jones <al2o3cr@gmail.com>
2009-08-31 00:16:14 -0400

    add support for a modified version of ! - crops the image to make it exactly the specified dimensions

--- lib/geometry.rb
+++ lib/geometry.rb
@@ -3,7 +3,7 @@
 # Used so I can use spiffy RMagick geometry strings with ImageScience
 class Geometry
   # ! and @ are removed until support for them is added
-  FLAGS = ['', '%', '<', '>']#, '!', '@']
+  FLAGS = ['', '%', '<', '>', '!']#, '@']
   RFLAGS = { '%' => :percent,
              '!' => :aspect,
              '<' => :>,
@@ -54,6 +54,9 @@ class Geometry
     new_height = orig_height
 
     case @flag
+      when :aspect
+        new_width = @width unless @width.nil? || @width.zero?
+        new_height = @height unless @height.nil? || @height.zero?
       when :percent
         scale_x = @width.zero?  ? 100 : @width
         scale_y = @height.zero? ? @width : @height
